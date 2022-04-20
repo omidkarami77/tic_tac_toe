81ec463ced91a21c294a54711f4f63368e33cb52
omidkarami77 <omidtkd77@gmail.com>
2022-04-20 17:06:48 +0330

    Implementation Turn

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+
+class _HomeState extends State<Home> {
+  bool isTurnO = true;
 
   @override
   Widget build(BuildContext context) {
@@ -16,95 +24,102 @@ class Home extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
               getScoreBoard(),
-              SizedBox(height: 40,),
+              SizedBox(
+                height: 40,
+              ),
               getGridView(),
               getTurn(),
-
             ],
           ),
         ));
   }
 
   Widget getTurn() {
-    return
-      Text(
-        'نوبت x',
-        style: TextStyle(
-            color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),);
+    return Text(
+      isTurnO ? 'Turn O' : 'Turn X',
+      style: TextStyle(
+          color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+    );
   }
 
-}
+  void tapped(int index) {
+    setState(() {
+      isTurnO = !isTurnO;
+    });
 
-Widget getGridView() {
-  return Expanded(
-    child: GridView.builder(
-        itemCount: 9,
-        gridDelegate:
-        SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-            onTap: (){
-              tapped(index);
-            },
-            child: Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
+    print('clicked ${index}');
+  }
+
+
+  Widget getGridView() {
+    return Expanded(
+      child: GridView.builder(
+          itemCount: 9,
+          gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                tapped(index);
+              },
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                ),
               ),
-            ),
-          );
-        }),
-  );
-}
+            );
+          }),
+    );
+  }
 
-void tapped(int index){
-  print('clicked ${index}');
-
-}
-Widget getScoreBoard() {
-  return Row(
-    children: [
-      Expanded(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(8),
-              child: Text(
-                'player O',
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-            ),
-            Padding(
+  Widget getScoreBoard() {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
                 padding: EdgeInsets.all(8),
                 child: Text(
-                  '0',
+                  'player O',
                   style: TextStyle(color: Colors.white, fontSize: 25),
-                )),
-          ],
-        ),
-      ),
-      Expanded(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(8),
-              child: Text(
-                'player X',
-                style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
               ),
-            ),
-            Padding(
+              Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    '0',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  )),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
                 padding: EdgeInsets.all(8),
                 child: Text(
-                  '1',
+                  'player X',
                   style: TextStyle(color: Colors.white, fontSize: 25),
-                )),
-          ],
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    '1',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  )),
+            ],
+          ),
         ),
-      ),
-    ],
-  );
+      ],
+    );
+  }
 }
